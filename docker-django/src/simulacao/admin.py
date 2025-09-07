40a9d6a73da19d7889be1f2b2e0c899106484f73
luiz-henrique-souza-santos <luizsouzaa080@gmail.com>
2025-09-07 14:41:29 -0300

    Cógido mais atualizado e integrado

--- docker-django/src/simulacao/admin.py
+++ docker-django/src/simulacao/admin.py
@@ -1,17 +1,2 @@
 from django.contrib import admin
-from .models import SimulacaoExecucao, SimulacaoPeriodo
 
-@admin.register(SimulacaoExecucao)
-class ExecucaoAdmin(admin.ModelAdmin):
-    list_display = ('requested_at', 'jogo', 'acao', 'lote_id', 'requested_by')
-    list_filter = ('acao', 'requested_at')
-    search_fields = ('jogo__name', 'jogo__cod', 'lote_id')
-
-@admin.register(SimulacaoPeriodo)
-class PeriodoAdmin(admin.ModelAdmin):
-    list_display = ('requested_at', 'jogo', 'acao', 'periodo_de', 'periodo_para', 'lote', 'step_index')
-    list_filter = ('acao', 'requested_at')
-    search_fields = ('jogo__name', 'jogo__cod', 'execucao__lote_id')
-
-    def lote(self, obj):
-        return obj.execucao.lote_id
--- docker-django/src/simulacao/forms.py
+++ docker-django/src/simulacao/forms.py
@@ -2,25 +2,32 @@ from django import forms
 from jogos.models import Jogo
 from .models import SimulacaoPeriodo
 
+
 class SimularForm(forms.Form):
-    jogos = forms.ModelMultipleChoiceField(
-        queryset=Jogo.objects.none(),
-        widget=forms.CheckboxSelectMultiple
-    )
     acao = forms.ChoiceField(
+        label="Ação",
         choices=SimulacaoPeriodo.ACAO_CHOICES,
         widget=forms.Select(attrs={"class": "form-select"})
     )
-    request_id = forms.CharField(required=False, widget=forms.HiddenInput)
 
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.fields["jogos"].queryset = Jogo.objects.filter(status=Jogo.ATIVO).order_by("name")
+    forcar_decisoes_automaticas = forms.BooleanField(
+        label="Forçar decisões automáticas",
+        required=False
+    )
 
-    def clean(self):
-        cleaned = super().clean()
-        if not cleaned.get("jogos"):
-            raise forms.ValidationError("Selecione ao menos um jogo.")
-        if not cleaned.get("acao"):
-            raise forms.ValidationError("Selecione uma ação.")
-        return cleaned
+    request_id = forms.CharField(
+        widget=forms.HiddenInput(),
+        required=False
+    )
+
+    jogos = forms.ModelMultipleChoiceField(
+    label="Selecione os jogos",
+    queryset=Jogo.objects.none(),
+    widget=forms.CheckboxSelectMultiple
+)
+
+    def init(self, args, **kwargs):
+        super().init(args, **kwargs)
+        self.fields["jogos"].queryset = (
+            Jogo.objects.filter(status=getattr(Jogo, "ATIVO", "A")).order_by("nome")
+        )
